c9fb4bd4474153e95fcaca7be3c12e6010c802c8
Goldiistn <54826508+Goldi-istn@users.noreply.github.com>
2020-01-13 09:54:49 +0700

    Add files via upload

---
+++ kasus.py
@@ -0,0 +1,5 @@
+umur = int (input("umur="))
+if umur >15 :
+    print ("dewasa")
+else :
+    print ("anakanak")
---
+++ rekursif1.py
@@ -0,0 +1,18 @@
+a=3; b=5
+def init():
+    a=0; b=0
+def tambah():
+        return a+b
+def kurang():
+            return a-b
+def cetak (n):
+                print (n)
+
+init ()
+print ("a=",a)
+print ("b=",b)
+print (tambah ())
+print (kurang())
+cetak (8+tambah())
+cetak (8-kurang())
+                
